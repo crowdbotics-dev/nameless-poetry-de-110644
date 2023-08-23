fc71005bb04752395daa880528f7dcd14ab6bc40
Crowdbotics <engineering@crowdbotics.com>
2023-08-23 00:52:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nameless-poetry-de-110644",
+  url: "https://nameless-poetry-de-110644.botics.co",
+  api: "https://nameless-poetry-de-110644.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
